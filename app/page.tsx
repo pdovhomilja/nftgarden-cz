21b6d7491090060be85f6836b7f02bffaed7a928
Pavel Dovhomilja <pavel@dovhomilja.cz>
2024-06-13 16:18:17 +0200

    fix: css update on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center w-full h-screen bg-black ">
+    <main className="flex flex-col text-white justify-center items-center w-full h-screen bg-black space-y-5 ">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-red-500">NFT Garden</h1>
-        <p className="text-white">A place to grow your NFTs</p>
+        <p className="text-2xl">A place to grow your NFTs</p>
         <p>We are working on it!</p>
       </div>
       <div>
