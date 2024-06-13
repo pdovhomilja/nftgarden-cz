f9c1864f3dfc45a97044a7b14c12b514653a3aa1
Pavel Dovhomilja <pavel@dovhomilja.cz>
2024-06-13 16:01:15 +0200

    feat: update home

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-red-500">NFT Garden</h1>
         <p className="text-white">A place to grow your NFTs</p>
+        <p>We are working on it!</p>
       </div>
     </main>
   );
